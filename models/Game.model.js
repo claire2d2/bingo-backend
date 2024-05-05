8c9debb2070b647c30cd75b4d05d2bd9643752e3
Claire Song <claireyuan.song@gmail.com>
2024-05-05 22:38:07 +0200

    initial commit

---
+++ models/Game.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const gameSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Game = model("Game", gameSchema);
+
+module.exports = Game;
